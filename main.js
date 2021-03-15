61d9084aff6742607202f4f04bf8f35b3e917584
propgmachine <77746698+propgmachine@users.noreply.github.com>
2021-03-15 09:46:45 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,43 @@
 // Your code here.
 
+function getFirstName(person) {
+  return person.firstName;
+}
 
+function getLastName(person) {
+  return person.lastName;
+}
 
+function getFullName(person) {
+  return '${person.firstName} ${person.lastName}';
+}
 
+function getFirstName(person, newFirstName) {
+  person.firstName; newFirstName;
+}
+
+function setAge(person, newAge) {
+  person.age = newAge;
+}
+
+function marry(person1, person2) {
+  person1.married = true;
+  person2.married = true;
+  person1.spouseName = getFullName(person2)
+  person2.spouseName = getFullName(person1)
+}
+
+function divorce(person1, person2) {
+  person1.married = false;
+  person2.married = false;
+  delete person1.spouseName;
+  delete person2.spouseName; 
+}
+
+const user = {
+  firstName; 'Steve'
+  lastName;
+};
 
 // Our code here. Don't touch!
 if (typeof getFirstName === 'undefined') {
